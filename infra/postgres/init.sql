99993336faf696ea4ee1542f05e2e4413dc4fd40
joaovitormessias <joaovitormessiasdamasio@gmail.com>
2026-05-11 23:58:25 -0300

    (create): upload init.sql
    Cria tabelas do banco de dados vetorial

---
+++ infra/postgres/init.sql
@@ -0,0 +1,22 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS documents (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS document_chunks (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  document_id UUID REFERENCES documents(id) ON DELETE CASCADE,
+  document_name TEXT NOT NULL,
+  page_number INTEGER,
+  content TEXT NOT NULL,
+  embedding VECTOR(1536) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS document_chunks_embedding_hnsw_idx
+ON document_chunks
+USING hnsw (embedding vector_cosine_ops);
