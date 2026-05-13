2ec0ba6f06a0bb1af97b3851672fbdb3d767ebfd
joaovitormessias <joaovitormessiasdamasio@gmail.com>
2026-05-13 18:56:10 -0300

    (update): adicionando comentario

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+
+// Conexao com backend usando variavel de ambiente configuravel, caso nao tenha o padrao eh a porta:3001
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3001",
 });
